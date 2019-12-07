a6673cb336cecc8fcdfeb8baf0973b1d62157526
Varoon Silochan <varoonx@gmail.com>
2019-12-07 10:03:44 -0400

    Blog template- 2:48

---
+++ src/templates/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../component/layout'
+const Blog = () => {
+    return (
+        <Layout>
+            This is the blog template
+        </Layout>
+    )
+}
+
+export default Blog
